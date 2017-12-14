2c3eb5f632d5f074043db70c937d95a1653508eb
daniel <zhao0829wang@163.com>
2017-12-14 11:04:56 +0800

    添加跨平台的workerman

---
+++ src/autoloader.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/11/21 0021
+ * Time: 20:55
+ */
+\toolbox\loader\DiffPlatformClassLoader::addClassloader(__DIR__,"Workerman");
